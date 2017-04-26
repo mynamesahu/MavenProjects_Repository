546be61d17e800e2c817145d386685866b622cc9
Sambit <Sambit@192.168.1.4>
2017-04-26 17:00:16 +0530

    new Maven project committed

---
+++ JavaApplicationProjects/Maven-JavaApplicationProject2/src/main/java/com/sambit/maven/myTestPackage/Employee.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sambit.maven.myTestPackage;
+
+/**
+ *
+ * @author Sambit
+ */
+public class Employee {
+    
+    public static void main (String argrs[]){
+        System.out.println("Employee Class");
+    }
+    
+}
